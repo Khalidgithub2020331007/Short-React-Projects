913ff491bbbf8c7227f4737bc6555e1b00939466
Khalid Azad <sheikhkhalidazad@gmail.com>
2025-10-05 11:46:34 +0600

    add childern props

--- react with typescript/typing_children_props/src/App.tsx
+++ react with typescript/typing_children_props/src/App.tsx
@@ -1,13 +1,15 @@
 
 import './App.css'
-import Button from './components/Button'
+// import Button from './components/Button'
+import Post from './components/Post'
 
 function App() {
 
   return (
     <div>
       <h1>Children props</h1>
-      <Button>Click Me</Button>      
+      {/* <Button>Click Me</Button>       */}
+      <Post></Post>
     </div>
   )
 }
---
+++ react with typescript/typing_children_props/src/components/Post.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './Card'
+
+const Post = () => {
+  return (
+    <div>
+        <Card>
+               <h1>Post Title1</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam dolor optio tenetur ad repudiandae nihil nisi dolorum nostrum delectus impedit.</p>
+  
+        </Card>
+    </div>
+  )
+}
+
+export default Post
